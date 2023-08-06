1de18f04944daceda26b54443643701524036d6f
MissCheptoo <mercy.cheptoo.data@gmail.com>
2023-08-06 18:22:24 +0300

    first commit

---
+++ prints.py
@@ -0,0 +1,18 @@
+#writing my first Hello world
+print("Hello world!")
+
+#more sophisticated print statement
+#print("I am a girl")
+print('I am 21 years old!')
+
+#another print statement
+print("I working at Elewa Tech Academy. I am a new instructor. I love my job!")
+
+#name
+print("My name is Mercy Cheptoo")
+
+#age
+print("I am 21 years old!")
+
+#expectations
+print("My expectations of today's class is to learn print statements!")
---
+++ variables.py
@@ -0,0 +1,22 @@
+#understanding variables in python
+#I will be assigning diferent variables to differnt values
+
+#two things that I love most in life
+Item_1 = "money"
+Item_2 = "food"
+
+#two reasons why I want to learn coding
+Reason_1 = "To become a data scientist"
+Reason_2 = "To code with my boyfriend"
+
+#two things that give me motivation
+Motivation_1 = "Money"
+Motivation_2 = "Good life"
+
+print(Item_1)
+print(Item_2)
+print(Reason_1)
+print(Reason_2)
+print(Motivation_1)
+print(Motivation_2)
+
