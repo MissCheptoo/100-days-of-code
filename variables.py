07da913513026fdad5505a44fd2d9d5a95b059eb
MissCheptoo <mercy.cheptoo.data@gmail.com>
2023-08-08 22:40:41 +0300

    first commit

--- variables.py
+++ variables.py
@@ -2,21 +2,114 @@
 #I will be assigning diferent variables to differnt values
 
 #two things that I love most in life
-Item_1 = "money"
-Item_2 = "food"
+item_1 = "money"
+item_2 = "food"
 
 #two reasons why I want to learn coding
-Reason_1 = "To become a data scientist"
-Reason_2 = "To code with my boyfriend"
+reason_1 = "To become a data scientist"
+reason_2 = "To code with my boyfriend"
 
 #two things that give me motivation
-Motivation_1 = "Money"
-Motivation_2 = "Good life"
+motivation_1 = "Money"
+motivation_2 = "Good life"
 
-print(Item_1)
-print(Item_2)
-print(Reason_1)
-print(Reason_2)
-print(Motivation_1)
-print(Motivation_2)
+print(item_1)
+print(item_2)
+print(reason_1)
+print(reason_2)
+print(motivation_1)
+print(motivation_2)
+
+x = 22 
+X = 33
+print(x)
+print(X)
+print(x)
+
+type (x)
+
+
+x,y,z = 'chocolate', 'vanilla',  'caramel'
+
+print(x)
+print(y)
+print(z)
+
+x = y = z = 'caremel chocolate'
+
+print(x)
+print(y)
+print(z)
+
+ice_cream = ['chocolate', 'vanilla', 'caramel']
+x, y ,z = ice_cream
+
+print(x)
+print(y)
+print(z)
+
+x = 'ice cream'
+y = ' is'
+z = ' my favorite'
+
+print(x+y+z)
+
+x = '1'
+y = '2'
+z = '3'
+
+print(x+y+z)
+
+x = 1
+y = 2
+z = 3
+
+
+print(x+y+z)
+
+age = 21
+
+print("you are " + str(age) + " years old")
+
+print("you are", age, "years old")
+ 
+#float 
+gpa = 3.5
+distance = 2.5
+price = 9.55
+print(f"your gpa is {gpa}")
+print(f"you ran {distance}km")
+print(f"the book cost {price}k sh")
+print(f"the book cost ${price}")
+
+ #strings
+food = "chicken"
+email = "mercycheptoo@gmail.com"
+name = "cheptoo"
+
+print(f"I love {food} very much")
+print(f"My email is : {email}")
+print(f"My name is {name}")
+
+x = 100
+y = 200
+
+print(y - x)
+print (y * y)
+type(x)
+
+name = "cheptoo"
+name.upper()
+
+x = 20
+y = 'simple'
+print(x, y )
+
+name = "cheptoo"
+length = len(name)
+print(length)
+name [0]
+
+print
+ 
 
